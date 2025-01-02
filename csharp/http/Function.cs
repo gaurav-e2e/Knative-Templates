4dcda70726a1187f0aae40b0b0321f1d38b2a995
Gaurav Bhinda <gaurav.bhinda@e2enetworks.com>
2025-01-02 11:59:20 +0530

    Commit changes

--- csharp/http/Function.cs
+++ csharp/http/Function.cs
@@ -1,17 +1,10 @@
-namespace Function
+using System;
+
+public class Function
 {
-    public class Function
+    public string Handle(string input)
     {
-        public IResult Handle(HttpRequest request)
-        {
-            // Get query parameters or body content as needed
-            // Example: var name = request.Query["name"].ToString() ?? "World";
-            
-            // Return a simple JSON response
-            return Results.Json(new { 
-                message = "Hello from C# Function!",
-                timestamp = DateTime.UtcNow
-            });
-        }
+        return $"Hello, {input}!";
     }
 }
+
